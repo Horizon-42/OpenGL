1855861dd260c9004a7082254c03e2f7610c54fb
horizon <liudxp@live.cn>
2018-05-21 15:55:53 +0800

    Initial Commit

---
+++ EX1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EX1
+//
+//  Created by 刘东旭 on 2018/5/21.
+//  Copyright © 2018年 刘东旭. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
